35ddcd135e2fd028da24ae14c57569ec37d5b33a
Tiwariji01 <akashtiwari19bca014@gmail.com>
2025-05-13 16:15:47 +0530

    fixed css missconception

--- script.js
+++ script.js
@@ -6,23 +6,17 @@ document.addEventListener("DOMContentLoaded", () => {
     };
   
     const totalPriceElement = document.getElementById("total-price");
-    const allCardBodies = document.querySelectorAll(".card-body");
     const allInputs = document.querySelectorAll("input[name='unit']");
-  
-    // Hide all card bodies initially
-    allCardBodies.forEach(body => body.style.display = "none");
   
     allInputs.forEach(input => {
       input.addEventListener("change", () => {
         const selectedValue = input.value;
   
         // Hide all card bodies
-        allCardBodies.forEach(body => body.style.display = "none");
+        allInputs.forEach( element => element.closest(".option").classList.remove("active"));
   
         // Show card body related to the selected radio input
-        const selectedCard = input.closest(".card");
-        const selectedBody = selectedCard.querySelector(".card-body");
-        selectedBody.style.display = "block";
+        input.closest(".option").classList.add("active")
   
         // Update total price
         totalPriceElement.textContent = prices[selectedValue];
